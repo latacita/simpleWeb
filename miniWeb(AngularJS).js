2fa80dc16f7b8c22882cd941e288800f89dc21a4
Pablo Sánchez <pablo.sanchez.barreiro@gmail.com>
2017-05-05 17:05:51 +0200

    Add bindings to AngularJS component
    Bindings to allow input and output parameter passing has been added to the Angular component, illustrating how components can interact between them.

--- miniWeb(AngularJS).js
+++ miniWeb(AngularJS).js
@@ -1,5 +1,15 @@
 var miniwebApp = angular.module('miniWeb',['simpleMenu']);
 
 miniwebApp.controller('miniWebController', function miniWebController($scope) {
-    
+
+    $scope.menuEntries =
+    [ { 'name':'Azul', 'value':'blue'},
+      { 'name':'Negro', 'value':'black'},
+      { 'name':'Rojo', 'value':'red'}
+    ];
+
+    this.onMenuEntryActivated = function(colorValue) {
+      $scope.activeColor = colorValue;
+    }
+
 });
--- simpleMenu/simpleMenu.component.angularJS.js
+++ simpleMenu/simpleMenu.component.angularJS.js
@@ -2,16 +2,18 @@ angular.module('simpleMenu',[]).component('simpleMenu',{
   templateUrl: 'simpleMenu/simpleMenu.template.html',
   controller: function SimpleMenuController($scope)
     {
-      this.menuEntries =
-        [ { 'name':'Negro', 'value':'black'},
-          { 'name':'Azul', 'value':'blue'},
-          { 'name':'Rojo', 'value':'red'}
-        ];
-
-        this.activeButton = this.menuEntries[0].value;
-
-        this.buttonClicked = function(colorValue) {
-          this.activeButton = colorValue;
+        this.buttonClicked = function(value) {
+          this.activeButton = value;
+          this.onEntryChanged({entryValue:value});
         };
+
+        this.$onInit = function() {
+          this.activeButton = this.menuEntries[0].value;
+          this.onEntryChanged({entryValue:this.activeButton});
+        }
+    },
+    bindings : {
+      menuEntries : '<',
+      onEntryChanged : '&'
     }
 });
